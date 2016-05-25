bc36e8b5fad2a03548f1ae4ec9c2037706686baf
attheodo <at@atworks.gr>
2016-05-25 20:03:02 +0300

    added files

---
+++ Example/Pods/Target Support Files/ATHExtensions/ATHExtensions-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ATHExtensions : NSObject
+@end
+@implementation PodsDummy_ATHExtensions
+@end
---
+++ Example/Pods/Target Support Files/ATHExtensions/ATHExtensions-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double ATHExtensionsVersionNumber;
+FOUNDATION_EXPORT const unsigned char ATHExtensionsVersionString[];
+
